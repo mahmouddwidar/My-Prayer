5e0018669d5bafd3864ccfc8a8ff5bca27a1de89
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2024-07-24 21:46:04 +0300

    fix: bug when the next prayer is fajr it's giving the remamning time with negative value

--- popup/content.js
+++ popup/content.js
@@ -1,145 +1,161 @@
 document.addEventListener("DOMContentLoaded", () => {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    } else {
-        console.log("Geolocation is not supported by your browser");
-    }
-
-    function onSuccess(position) {
-        const { latitude, longitude } = position.coords;
-        chrome.storage.local.set(
-            { latitude: latitude, longitude: longitude },
-            () => {
-                console.log("Location data saved");
-            }
-        );
-    }
-
-    function onError(error) {
-        console.error("Error getting location:", error);
-        console.log("Unable to retrieve your location");
-    }
-
-    let todayPrayerTimes;
-    let todayDates;
-
-    async function getAllTimings() {
-        const result = await chrome.storage.local.get(["timings"]);
-        const dates = await chrome.storage.local.get(['hijri_day', 'hijri_month', 'gregorian_day']);
-        todayTimings = result["timings"];
-        todayDates = dates;
-        todayPrayerTimes = [
-            { name: "Fajr", time: convertTo12HourFormat(todayTimings.Fajr) },
-            { name: "Sunrise", time: convertTo12HourFormat(todayTimings.Sunrise) },
-            { name: "Dhuhr", time: convertTo12HourFormat(todayTimings.Dhuhr) },
-            { name: "Asr", time: convertTo12HourFormat(todayTimings.Asr) },
-            { name: "Maghrib", time: convertTo12HourFormat(todayTimings.Maghrib) },
-            { name: "Isha", time: convertTo12HourFormat(todayTimings.Isha) },
-        ];
-    }
-
-    getAllTimings().then(() => {
-        updatePrayerTimes(todayPrayerTimes);
-		updateDates(todayDates);
-    });
-
-    function updatePrayerTimes(prayerTimes) {
-        const currentTime = new Date();
-        let nextPrayer, previousPrayer;
-
-        for (let i = 0; i < prayerTimes.length; i++) {
-            const prayerDate = parseTimeToDate(prayerTimes[i].time, prayerTimes[i].name);
-            if (currentTime < prayerDate) {
-                nextPrayer = prayerTimes[i];
-                previousPrayer = i === 0 ? prayerTimes[prayerTimes.length - 1] : prayerTimes[i - 1];
-                break;
-            }
-        }
-
-        if (!nextPrayer) {
-            nextPrayer = prayerTimes[0];
-            previousPrayer = prayerTimes[prayerTimes.length - 1];
-        }
-
-        document.getElementById("previous-time").textContent = `${previousPrayer.time}`;
-        document.getElementById("previousPrayer").textContent = `${previousPrayer.name}`;
-        document.getElementById("next-time").textContent = `${nextPrayer.time}`;
-        document.getElementById("nextPrayer").textContent = `${nextPrayer.name}`;
-
-        updateProgressBar(
-            currentTime,
-            parseTimeToDate(nextPrayer.time),
-            parseTimeToDate(previousPrayer.time)
-        );
-    }
-
-	function updateDates(dates) {
-		document.getElementById('day').textContent = `${dates.gregorian_day}`;
-		document.getElementById('date').textContent = `${dates.hijri_day}`;
-		document.getElementById('month').textContent = `${dates.hijri_month}`;
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	} else {
+		alert("Geolocation is not supported by your browser");
+		console.log("Geolocation is not supported by your browser");
 	}
 
-    function parseTimeToDate(time) {
-        const [timeString, period] = time.split(" ");
-        const [hours, minutes] = timeString.split(":");
-        let hour = parseInt(hours);
-        if (period === "PM" && hour !== 12) hour += 12;
-        if (period === "AM" && hour === 12) hour = 0;
+	function onSuccess(position) {
+		const { latitude, longitude } = position.coords;
+		chrome.storage.local.set(
+			{ latitude: latitude, longitude: longitude },
+			() => {
+				console.log("Location data saved");
+			}
+		);
+	}
 
-        const date = new Date();
-        date.setHours(hour, parseInt(minutes), 0, 0);
+	function onError(error) {
+		console.error("Error getting location:", error);
+		console.log("Unable to retrieve your location");
+	}
 
-        return date;
-    }
+	let todayPrayerTimes;
+	let todayDates;
 
-    function convertTo12HourFormat(time24) {
-        const [hours, minutes] = time24.split(":");
-        let period = "AM";
-        let hour = parseInt(hours);
+	async function getAllTimings() {
+		const result = await chrome.storage.local.get(["timings"]);
+		const dates = await chrome.storage.local.get([
+			"hijri_day",
+			"hijri_month",
+			"gregorian_day",
+		]);
+		todayTimings = result["timings"];
+		todayDates = dates;
+		todayPrayerTimes = [
+			{ name: "Fajr", time: convertTo12HourFormat(todayTimings.Fajr) },
+			{ name: "Sunrise", time: convertTo12HourFormat(todayTimings.Sunrise) },
+			{ name: "Dhuhr", time: convertTo12HourFormat(todayTimings.Dhuhr) },
+			{ name: "Asr", time: convertTo12HourFormat(todayTimings.Asr) },
+			{ name: "Maghrib", time: convertTo12HourFormat(todayTimings.Maghrib) },
+			{ name: "Isha", time: convertTo12HourFormat(todayTimings.Isha) },
+		];
+	}
 
-        if (hour >= 12) {
-            period = "PM";
-        }
-        if (hour === 0) {
-            hour = 12;
-        } else if (hour > 12) {
-            hour -= 12;
-        }
+	getAllTimings().then(() => {
+		updatePrayerTimes(todayPrayerTimes);
+		updateDates(todayDates);
+	});
 
-        return `${hour}:${minutes} ${period}`;
-    }
+	function updatePrayerTimes(prayerTimes) {
+		const currentTime = new Date();
+		let nextPrayer, previousPrayer;
 
-    function updateProgressBar(currentTime, nextPrayerTime, previousPrayerTime) {
-        const timeDiff = nextPrayerTime - currentTime;
-    
-        let totalTime = nextPrayerTime - previousPrayerTime;
-    
-        // Adjust totalTime for overnight prayer times
-        if (totalTime < 0) {
-            totalTime += 24 * 60 * 60 * 1000;
-        }
-    
-        const progressBar = document.querySelector(".progress-bar");
-        const progressBarBackground = document.querySelector(".progress");
-        const progress = 100 - (timeDiff / totalTime) * 100;
-    
-        progressBar.style.width = `${progress}%`;
-    
-        const totalSeconds = Math.floor(timeDiff / 1000);
-        const hours = Math.floor(totalSeconds / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-    
-        if (hours > 0) {
-            progressBar.textContent = `${hours} hr ${minutes} min`;
-            progressBar.setAttribute("title", `${hours} hr ${minutes} min`);
-            progressBarBackground.setAttribute("title", `${hours} hr ${minutes} min`);
-        } else {
-            progressBar.textContent = `${minutes} min`;
-            progressBar.setAttribute("title", `${minutes} min`);
-            progressBarBackground.setAttribute("title", `${minutes} min`);
-        }
-    }
-    
+		for (let i = 0; i < prayerTimes.length; i++) {
+			const prayerDate = parseTimeToDate(
+				prayerTimes[i].time,
+				prayerTimes[i].name
+			);
+			if (currentTime < prayerDate) {
+				nextPrayer = prayerTimes[i];
+				previousPrayer =
+					i === 0 ? prayerTimes[prayerTimes.length - 1] : prayerTimes[i - 1];
+				break;
+			}
+		}
+
+		if (!nextPrayer) {
+			nextPrayer = prayerTimes[0];
+			previousPrayer = prayerTimes[prayerTimes.length - 1];
+		}
+
+		document.getElementById(
+			"previous-time"
+		).textContent = `${previousPrayer.time}`;
+		document.getElementById(
+			"previousPrayer"
+		).textContent = `${previousPrayer.name}`;
+		document.getElementById("next-time").textContent = `${nextPrayer.time}`;
+		document.getElementById("nextPrayer").textContent = `${nextPrayer.name}`;
+
+		updateProgressBar(
+			currentTime,
+			parseTimeToDate(nextPrayer.time),
+			parseTimeToDate(previousPrayer.time)
+		);
+	}
+
+	function updateDates(dates) {
+		document.getElementById("day").textContent = `${dates.gregorian_day}`;
+		document.getElementById("date").textContent = `${dates.hijri_day}`;
+		document.getElementById("month").textContent = `${dates.hijri_month}`;
+	}
+
+	function parseTimeToDate(time) {
+		const [timeString, period] = time.split(" ");
+		const [hours, minutes] = timeString.split(":");
+		let hour = parseInt(hours);
+		if (period === "PM" && hour !== 12) hour += 12;
+		if (period === "AM" && hour === 12) hour = 0;
+
+		const date = new Date();
+		date.setHours(hour, parseInt(minutes), 0, 0);
+
+		return date;
+	}
+
+	function convertTo12HourFormat(time24) {
+		const [hours, minutes] = time24.split(":");
+		let period = "AM";
+		let hour = parseInt(hours);
+
+		if (hour >= 12) {
+			period = "PM";
+		}
+		if (hour === 0) {
+			hour = 12;
+		} else if (hour > 12) {
+			hour -= 12;
+		}
+
+		return `${hour}:${minutes} ${period}`;
+	}
+
+	function updateProgressBar(currentTime, nextPrayerTime, previousPrayerTime) {
+		let timeDiff = nextPrayerTime - currentTime;
+
+		let totalTime = nextPrayerTime - previousPrayerTime;
+
+		// Adjust totalTime for overnight prayer times
+		if (totalTime < 0) {
+			totalTime += 24 * 60 * 60 * 1000;
+			if (currentTime > nextPrayerTime) {
+				timeDiff += 24 * 60 * 60 * 1000;
+				console.log("time diff updated.");
+			}
+		}
+
+		const progressBar = document.querySelector(".progress-bar");
+		const progressBarBackground = document.querySelector(".progress");
+		const progress = 100 - (timeDiff / totalTime) * 100;
+
+		progressBar.style.width = `${progress}%`;
+
+		const totalSeconds = Math.floor(timeDiff / 1000);
+		const hours = Math.floor(totalSeconds / 3600);
+		const minutes = Math.floor((totalSeconds % 3600) / 60);
+
+		if (hours > 0) {
+			progressBar.textContent = `${hours} hr ${minutes} min`;
+			progressBar.setAttribute("title", `${hours} hr ${minutes} min`);
+			progressBarBackground.setAttribute("title", `${hours} hr ${minutes} min`);
+		} else {
+			progressBar.textContent = `${minutes} min`;
+			progressBar.setAttribute("title", `${minutes} min`);
+			progressBarBackground.setAttribute("title", `${minutes} min`);
+		}
+	}
 
 	setInterval(() => {
 		updatePrayerTimes(todayPrayerTimes);
